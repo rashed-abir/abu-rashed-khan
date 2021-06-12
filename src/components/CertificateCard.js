4ffe59db8f6db64040b790653d59f3795b6a78de
Rashed Abir <rashed15-2155@diu.edu.bd>
2021-06-12 20:07:35 +0600

    certificate

--- src/components/CertificateCard.js
+++ src/components/CertificateCard.js
@@ -2,29 +2,22 @@ import React from "react";
 
 function CertificateCard({ data, light }) {
   return (
-    <div
-      className={
-        light
-          ? "resume-card_body_light certificate"
-          : "resume-card_body certificate"
-      }
-      key={data.heading}
-    >
-      <p
-        className="resume-card_name"
-        style={{ fontWeight: "500", margin: "auto" }}
+    <ul className="list-group mb-1">
+      <li
+        className={
+          light
+            ? "list-group-item d-flex justify-content-between align-items-center certificate_light"
+            : "list-group-item d-flex justify-content-between align-items-center certificate_dark"
+        }
       >
         {data.heading}
-      </p>
-      <a
-        href={data.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={light ? "certificate_light" : "certificate_dark"}
-      >
-        (verify)
-      </a>
-    </div>
+        <span className="badge bg-primary rounded-pill certificate_view">
+          <a target="_blank" rel="noreferrer" className="text-white" href={data.link}>
+            view
+          </a>
+        </span>
+      </li>
+    </ul>
   );
 }
 
